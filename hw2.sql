534f3002be6125fcede2c7f46bf465c83d6c1bbb
OksanaSha <oksanashatalova@bk.ru>
2021-11-26 18:44:19 +0300

    Scheme and sql

---
+++ hw2.sql
@@ -0,0 +1,53 @@
+create table if not exists Genres (
+id serial primary key,
+name varchar(40) not null unique,
+description text
+);
+
+create table if not exists Musicians (
+id serial primary key,
+name varchar(40) not null unique,
+biography text
+);
+
+create table if not exists GenreMusicians (
+genre_id integer references Genres(id),
+musician_id integer references Musicians(id),
+primary key (genre_id, musician_id)
+);
+
+create table if not exists Albums (
+id serial primary key,
+name varchar(40) not null,
+year integer not null
+);
+
+create table if not exists MusicianAlbums (
+musician_id integer references Musicians(id),
+album_id integer references Albums(id),
+primary key (musician_id, album_id)
+);
+
+create table if not exists Tracks (
+id serial primary key,
+name varchar(40) not null,
+duration_sec integer not null
+);
+
+create table if not exists AlbumTracks (
+album_id integer references Albums(id),
+track_id integer references Tracks(id),
+primary key (album_id, track_id)
+);
+
+create table if not exists Collections (
+id serial primary key,
+name varchar(60) not null,
+year integer not null
+);
+
+create table if not exists CollectionTracks (
+collection_id integer references Collections(id),
+track_id integer references Tracks(id),
+primary key (collection_id, track_id)
+);
